4204ab5c2d3ed69f0bc668d786ec83a85a675c50
Ana <36892125+nanuk22@users.noreply.github.com>
2022-05-16 17:00:45 +0000

    Completed app

--- index.js
+++ index.js
@@ -8,25 +8,46 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get("/api/:date?", function(req, res) {
+  let data = req.params.date;
+  let date;
+  if (Date.parse(data)) {
+    date = data;
+    req.timeStr = new Date(date).toUTCString();
+    req.timeMs = new Date(date).getTime();
+  } else if (Number(data)) {
+    date = Number(data);
+    req.timeStr = new Date(date).toUTCString();
+    req.timeMs = new Date(date).getTime();
+  } else if (data == undefined) {
+    req.timeStr = new Date().toUTCString();
+    req.timeMs = new Date().getTime();
+  } else {
+    return res.json({ error : "Invalid Date" });
+  }
+  res.json({ "unix": req.timeMs, "utc": req.timeStr });
+});
+
